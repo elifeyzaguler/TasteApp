c6307b5e3de5377d2cda08033cd2edc1cb537fba
Elif Feyza Güler <74074370+elifeyzaguler@users.noreply.github.com>
2024-07-05 21:36:48 +0300

    CORS problem solved, get-me api added and callback function updated.

--- src/app.js
+++ src/app.js
@@ -6,10 +6,18 @@ const routes = require('./routes');
 const bodyParser = require('body-parser');
 const spotifyApi = require('./api/spotifyApi');
 
+const cors = require('cors');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cors({
+  origin: "http://localhost:3001",
+  //methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use('/', routes);
 
 app.get('/login', (req, res) => {
@@ -19,18 +27,49 @@ app.get('/login', (req, res) => {
 app.get('/callback', (req, res) => {
   const { code } = req.query;
   
-  if (code){
-    handleAuthorization(code).subscribe({next: (result) => {
+  // if (code){
+  //   handleAuthorization(code).subscribe({next: (result) => {
+  //       console.log(result);
+  //       res.send(result);
+  //     }, complete: ()=>{
+  //       spotifyApi.getMe()
+  //       .then(function(data) {
+  //         console.log('Some information about the authenticated user', data.body);
+  //       }, function(err) {
+  //         console.log('Something went wrong!', err);
+  //       });
+  //     }});
+  // }
+
+  if(code) {
+    handleAuthorization(code).subscribe({
+      next: (result) => {
         console.log(result);
-        res.send(result);
-      }, complete: ()=>{
+
+        spotifyApi.setAccessToken(result.body.access_token);
+        spotifyApi.setRefreshToken(result.body.refresh_token);
+
         spotifyApi.getMe()
-        .then(function(data) {
-          console.log('Some information about the authenticated user', data.body);
-        }, function(err) {
-          console.log('Something went wrong!', err);
-        });
-      }});
+          .then(function(data) {
+            console.log("Information about the authenticated user ", data);
+
+            res.redirect('http://localhost:3001/user-profile');
+          })
+          .catch(function(err) {
+            console.log('Something went wrong!', err);
+            res.send('An error occured.')
+          });
+      },
+      error: (err) => {
+        console.log('Error during authorization: ', err);
+        res.send('An error occured during authorization.');
+      },
+      complete: () => {
+        console.log('Authorization complete!');
+      }
+    });
+  } else {
+    res.send('No authorization code found.');
   }
 
 });
